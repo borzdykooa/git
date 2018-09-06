d560ffcb3e7b32cfbffaf5311b36b6a593efab19
borzdykooa <borzdykooa@mail.ru>
2018-09-06 16:29:45 +0300

    Story 182: Fix benchmarks for speed

---
+++ src/main/java/com/borzdykooa/App.java
@@ -0,0 +1,13 @@
+package com.borzdykooa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
